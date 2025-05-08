e5c5d9300bbed739186cfca7b93907214d80a681
Ingrid3ds2025 <thamires.carvalho.carneiro@escola.pr.gov.br>
2025-05-08 12:04:30 -0300

    Add files via upload

---
+++ src/tela.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+import { StyleSheet, Text, View } from 'react-native';
+
+class Tela extends Component {
+    render({
+        return{
+            <View>
+
+            </View>
+        }
+    })
+}
+
