2dc07b74ea2753788d2a6700248792ed5d23451d
Ingrid3ds2025 <thamires.carvalho.carneiro@escola.pr.gov.br>
2025-05-27 07:52:15 -0300

    Add files via upload

--- src/tela.jsx
+++ src/tela.jsx
@@ -2,6 +2,41 @@ import React, {Component} from "react";
 import { StyleSheet, Text, View } from 'react-native';
 
 class Tela extends Component {
-    
+    render(){
+        return(
+            <View style={styles.container}>
+                 <Text style={styles.logoTexto}>Logo</Text>
+
+                 <View>
+                    <Text style={styles.dadosTexto}>DADOS</Text>
+                 </View>
+            </View>
+        );
+    }
 }
+
+export default Tela;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'black',
+
+    },
+
+    logoTexto: {
+        color: 'white',
+        fontSize: 30,
+        fontWeight: 500,
+        fontFamily: 'Papyrus',
+    },
+
+    dadosTexto: {
+        color: 'white',
+        fontSize: 30,
+        fontWeight: 500,
+        fontFamily: 'Papyrus',
+
+    }
+})
 
